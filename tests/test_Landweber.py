c188ac0bc2598c2d580ae082ce61d43fdaa65231
Ratmir Miftachov <40966507+ratmir-miftachov@users.noreply.github.com>
2023-12-11 20:12:06 +0100

    test monotonicity residuals

--- tests/test_Landweber.py
+++ tests/test_Landweber.py
@@ -8,18 +8,34 @@ class Test_landweber(unittest.TestCase):
     def setUp(self):
         self.para_size = 10
         self.f = np.random.normal(0, 1, self.para_size)  
+        self.iteration = 30
         
     def test_elementary_estimation(self):
         X = np.eye(self.para_size)  
         Y = X @ self.f  
-        iter = 30
         alg = es.landweber(X, Y)
-        alg.landweber(iter)
+        alg.landweber(self.iteration)
         beta = alg.landweber_estimate
     
         for i in range(len(beta)):
             self.assertAlmostEqual(beta[i], self.f[i], places=7)
 
+
+    def test_monotonicity_residuals(self):
+        indices = np.arange(self.para_size)+1
+        X = np.diag(1/(np.sqrt(indices)))
+        f = 5*np.exp(-0.1*indices)
+        Y = X @ f
+        alg = es.landweber(X, Y)
+        alg.landweber(self.iteration)
+
+        for m in range((alg.iter - 1)):
+            self.assertTrue(alg.residuals[m] >= alg.residuals[(m + 1)])
+
+
+
+
  #Running the tests
-#if __name__ == '__main__':
-#    unittest.main()
+if __name__ == '__main__':
+    unittest.main()
+
