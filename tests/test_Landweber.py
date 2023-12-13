6363630aec6394e3e9e0390aacacaed5152a982a
Ratmir Miftachov <40966507+ratmir-miftachov@users.noreply.github.com>
2023-12-13 17:31:37 +0100

    test monotonicity bias2, var landweber

--- tests/test_Landweber.py
+++ tests/test_Landweber.py
@@ -6,34 +6,42 @@ class Test_landweber(unittest.TestCase):
     """Tests for the landweber algorithm"""
 
     def setUp(self):
-        self.para_size = 10
-        self.f = np.random.normal(0, 1, self.para_size)  
-        self.iteration = 30
+        self.para_size = 50
+        self.iteration = 100
         self.tol = 10**(-7)
+        #supersmooth signal
+        self.indices = np.arange(self.para_size)+1
+        self.X = np.diag(1/(np.sqrt(self.indices)))
+        self.f = 5*np.exp(-0.1*self.indices)
+        self.Y = self.X @ self.f
+        self.alg = es.Landweber(self.X, self.Y, true_signal = self.f, true_noise_level = 1)
+        self.alg.landweber(self.iteration)
         
     def test_elementary_estimation(self):
-        X = np.eye(self.para_size)
-        Y = X @ self.f
-        alg = es.landweber(X, Y)
+        identity_matrix = np.eye(self.para_size)
+        f = np.random.normal(0, 1, self.para_size)
+        Y = identity_matrix @ f
+        alg = es.Landweber(identity_matrix, Y)
         alg.landweber(self.iteration)
         beta = alg.landweber_estimate
 
         for i in range(len(beta)):
-            deviation = np.abs(beta[i] - self.f[i])
+            deviation = np.abs(beta[i] - f[i])
             self.assertTrue(deviation < self.tol)
 
     def test_monotonicity_residuals(self):
-        indices = np.arange(self.para_size)+1
-        X = np.diag(1/(np.sqrt(indices)))
-        f = 5*np.exp(-0.1*indices)
-        Y = X @ f
-        alg = es.landweber(X, Y)
-        alg.landweber(self.iteration)
+        
+        for m in range((self.alg.iter - 1)):
+            self.assertTrue(self.alg.residuals[m] >= self.alg.residuals[(m + 1)])
 
-        for m in range((alg.iter - 1)):
-            self.assertTrue(alg.residuals[m] >= alg.residuals[(m + 1)])
+    def test_monotonicity_of_bias_and_variance(self):
 
-
+        for i in range((self.alg.iter - 1)):
+            if self.alg.strong_bias2[i] >= self.tol:
+                self.assertTrue(self.alg.strong_bias2[i] >= self.alg.strong_bias2[(i + 1)])
+                self.assertTrue(self.alg.strong_variance[i] <= self.alg.strong_variance[(i + 1)])
+                self.assertTrue(self.alg.weak_bias2[i] >= self.alg.weak_bias2[(i + 1)])
+                self.assertTrue(self.alg.weak_variance[i] <= self.alg.weak_variance[(i + 1)])              
 
 
  #Running the tests
