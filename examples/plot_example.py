cfc4e8010edd8559ec8b559f128c279e5dee9f85
ziebelle <32039090+ziebelle@users.noreply.github.com>
2024-01-10 15:17:06 +0100

    Update plot_example.py (Add section headers as comments)

--- examples/plot_example.py
+++ examples/plot_example.py
@@ -11,11 +11,40 @@ import seaborn as sns
 sns.set_theme()
 import EarlyStopping as es
 
+# %%
+# Section 1 
+# ------------------------
+# This is the first section!
+# The `#%%` signifies to Sphinx-Gallery that this text should be rendered as
+# reST and if using one of the above IDE/plugin's, also signifies the start of a
+# 'code block'.
+
 sample_size = 10
 para_size   = 10
 cov = np.identity(para_size)
 X = np.random.multivariate_normal(np.zeros(para_size), cov, sample_size)
 beta = np.ones(sample_size)
+f = X @ beta
+Y = f
+alg = es.L2_boost(X, Y, f)
+
+fig = plt.figure(figsize = (10,7))
+plt.plot(beta)
+plt.show()
+
+# %%
+# Section 2
+# ------------------------
+# This is the first section!
+# The `#%%` signifies to Sphinx-Gallery that this text should be rendered as
+# reST and if using one of the above IDE/plugin's, also signifies the start of a
+# 'code block'.
+
+sample_size = 10
+para_size   = 10
+cov = np.identity(para_size)
+X = np.random.multivariate_normal(np.zeros(para_size), cov, sample_size)
+beta = 2*np.ones(sample_size)
 f = X @ beta
 Y = f
 alg = es.L2_boost(X, Y, f)
