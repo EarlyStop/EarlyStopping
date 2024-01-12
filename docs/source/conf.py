a28982f64b1c81b6efc6dc625a7c306d8101c546
ziebelle <32039090+ziebelle@users.noreply.github.com>
2024-01-12 18:53:46 +0100

    Update conf.py (Perfect recursion)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,14 @@ release = '0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx_gallery.gen_gallery',
-]        
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'python': ("https://docs.python.org/3", None),
+    'EarlyStopping': ("https://esfiep.github.io/EarlyStopping/",None),
+}
 
 sphinx_gallery_conf = {
      'examples_dirs': '../../examples',   # path to your example scripts
