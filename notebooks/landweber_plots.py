aa5613ddea7cbdbc30d0f82c9cf9e0a48c679c42
Ratmir Miftachov <40966507+ratmir-miftachov@users.noreply.github.com>
2024-01-12 16:08:20 +0100

    Add Weak/strong balanced oracle. Add docstrings

--- notebooks/landweber_plots.py
+++ notebooks/landweber_plots.py
@@ -14,6 +14,18 @@ signal_supersmooth = 5*np.exp(-0.1*indices)
 signal_smooth = 5000*np.abs(np.sin(0.01*indices))*indices**(-1.6)
 signal_rough = 250*np.abs(np.sin(0.002*indices))*indices**(-0.8)
 
+plt.figure(figsize=(10, 6))
+plt.plot(indices, signal_supersmooth, label="supersmooth signal")
+plt.plot(indices, signal_smooth, label="smooth signal")
+plt.plot(indices, signal_rough, label="rough signal")
+plt.ylabel("Signal")
+plt.xlabel("Index")
+plt.xlim([0,1000])
+plt.ylim([0,1.6])
+plt.legend()
+plt.show()
+
+
 NOISE_LEVEL = 0.01
 noise = np.random.normal(0, NOISE_LEVEL, D)
 
@@ -26,7 +38,7 @@ models_supersmooth = es.Landweber(design_matrix, observation_supersmooth, true_n
 models_smooth = es.Landweber(design_matrix, observation_smooth, true_noise_level=NOISE_LEVEL, true_signal=signal_smooth)
 models_rough = es.Landweber(design_matrix, observation_rough, true_noise_level=NOISE_LEVEL, true_signal=signal_rough)
 
-iter = 1000
+iter = 1500
 models_supersmooth.landweber_gather_all(iter)
 models_smooth.landweber_gather_all(iter)
 models_rough.landweber_gather_all(iter)
@@ -36,32 +48,43 @@ supersmooth_m = models_supersmooth.early_stopping_index
 smooth_m = models_smooth.early_stopping_index
 rough_m = models_rough.early_stopping_index
 
+# Weak balanced oracle
+supersmooth_weak_oracle = models_supersmooth.weak_balanced_oracle
+smooth_weak_oracle = models_smooth.weak_balanced_oracle
+rough_weak_oracle = models_rough.weak_balanced_oracle
+
+# Strong balanced oracle
+supersmooth_strong_oracle = models_supersmooth.strong_balanced_oracle
+smooth_strong_oracle = models_smooth.strong_balanced_oracle
+rough_strong_oracle = models_rough.strong_balanced_oracle
 
 fig, axs = plt.subplots(3, 1, figsize=(14, 8))
 
-axs[0].plot(range(0, iter+1), models_supersmooth.residuals)  
+axs[0].plot(range(0, iter+1), models_supersmooth.residuals)
 axs[0].axvline(x=supersmooth_m, color='red', linestyle='--')
-axs[0].set_xlim([0, 400])  
-axs[0].set_ylim([0, 100])  
+axs[0].set_xlim([0, 400])
+axs[0].set_ylim([0, 100])
 axs[0].set_xlabel('Iteration')
 axs[0].set_ylabel('Residuals')
 
 
-axs[1].plot(range(0, iter+1), models_supersmooth.strong_error, color='orange', label='Error')  
-axs[1].plot(range(0, iter+1), models_supersmooth.strong_bias2, label='$Bias^2$', color='grey')  
-axs[1].plot(range(0, iter+1), models_supersmooth.strong_variance, label='Variance', color='blue')  
+axs[1].plot(range(0, iter+1), models_supersmooth.strong_error, color='orange', label='Error')
+axs[1].plot(range(0, iter+1), models_supersmooth.strong_bias2, label='$Bias^2$', color='grey')
+axs[1].plot(range(0, iter+1), models_supersmooth.strong_variance, label='Variance', color='blue')
 axs[1].axvline(x=supersmooth_m, color='red', linestyle='--')
-axs[1].set_xlim([0, 400])  
-axs[1].set_ylim([0, 100])  
+axs[1].axvline(x=supersmooth_strong_oracle, color='blue', linestyle=':')
+axs[1].set_xlim([0, 400])
+axs[1].set_ylim([0, 100])
 axs[1].set_xlabel('Iteration')
 axs[1].set_ylabel('Strong Quantities')
 
-axs[2].plot(range(0, iter+1), models_supersmooth.weak_error, color='orange', label='Error')  
-axs[2].plot(range(0, iter+1), models_supersmooth.weak_bias2, label='$Bias^2$', color='grey')  
-axs[2].plot(range(0, iter+1), models_supersmooth.weak_variance, label='Variance', color='blue')  
+axs[2].plot(range(0, iter+1), models_supersmooth.weak_error, color='orange', label='Error')
+axs[2].plot(range(0, iter+1), models_supersmooth.weak_bias2, label='$Bias^2$', color='grey')
+axs[2].plot(range(0, iter+1), models_supersmooth.weak_variance, label='Variance', color='blue')
 axs[2].axvline(x=supersmooth_m, color='red', linestyle='--')
-axs[2].set_xlim([0, 400])  
-axs[2].set_ylim([0, 0.02])  
+axs[2].axvline(x=supersmooth_weak_oracle, color='blue', linestyle=':')
+axs[2].set_xlim([0, 400])
+axs[2].set_ylim([0, 0.02])
 axs[2].set_xlabel('Iteration')
 axs[2].set_ylabel('Weak Quantities')
 axs[2].legend()
@@ -73,28 +96,30 @@ plt.show()
 
 fig, axs = plt.subplots(3, 1, figsize=(14, 8))
 
-axs[0].plot(range(0, iter+1), models_smooth.residuals)  
+axs[0].plot(range(0, iter+1), models_smooth.residuals)
 axs[0].axvline(x=smooth_m, color='red', linestyle='--')
-axs[0].set_xlim([0, 500])  
-axs[0].set_ylim([0, 100])  
+axs[0].set_xlim([0, 500])
+axs[0].set_ylim([0, 100])
 axs[0].set_xlabel('Iteration')
 axs[0].set_ylabel('Residuals')
 
-axs[1].plot(range(0, iter+1), models_smooth.strong_error, color='orange', label='Error')  
-axs[1].plot(range(0, iter+1), models_smooth.strong_bias2, label='$Bias^2$', color='grey')  
-axs[1].plot(range(0, iter+1), models_smooth.strong_variance, label='Variance', color='blue')  
+axs[1].plot(range(0, iter+1), models_smooth.strong_error, color='orange', label='Error')
+axs[1].plot(range(0, iter+1), models_smooth.strong_bias2, label='$Bias^2$', color='grey')
+axs[1].plot(range(0, iter+1), models_smooth.strong_variance, label='Variance', color='blue')
 axs[1].axvline(x=smooth_m, color='red', linestyle='--')
-axs[1].set_xlim([0, 500])  
-axs[1].set_ylim([0, 100])  
+axs[1].axvline(x=smooth_strong_oracle, color='blue', linestyle=':')
+axs[1].set_xlim([0, 500])
+axs[1].set_ylim([0, 100])
 axs[1].set_xlabel('Iteration')
 axs[1].set_ylabel('Strong Quantities')
 
-axs[2].plot(range(0, iter+1), models_smooth.weak_error, color='orange', label='Error')  
-axs[2].plot(range(0, iter+1), models_smooth.weak_bias2, label='$Bias^2$', color='grey')  
-axs[2].plot(range(0, iter+1), models_smooth.weak_variance, label='Variance', color='blue')  
+axs[2].plot(range(0, iter+1), models_smooth.weak_error, color='orange', label='Error')
+axs[2].plot(range(0, iter+1), models_smooth.weak_bias2, label='$Bias^2$', color='grey')
+axs[2].plot(range(0, iter+1), models_smooth.weak_variance, label='Variance', color='blue')
 axs[2].axvline(x=smooth_m, color='red', linestyle='--')
-axs[2].set_xlim([0, 500])  
-axs[2].set_ylim([0, 0.5])  
+axs[2].axvline(x=smooth_weak_oracle, color='blue', linestyle=':')
+axs[2].set_xlim([0, 500])
+axs[2].set_ylim([0, 0.5])
 axs[2].set_xlabel('Iteration')
 axs[2].set_ylabel('Weak Quantities')
 axs[2].legend()
@@ -108,24 +133,26 @@ plt.show()
 
 fig, axs = plt.subplots(3, 1, figsize=(14, 8))
 
-axs[0].plot(range(0, iter+1), models_rough.residuals)  
+axs[0].plot(range(0, iter+1), models_rough.residuals)
 axs[0].axvline(x=rough_m, color='red', linestyle='--')
 axs[0].set_xlabel('Iteration')
 axs[0].set_ylabel('Residuals')
 
-axs[1].plot(range(0, iter+1), models_rough.strong_error, color='orange', label='Error')  
-axs[1].plot(range(0, iter+1), models_rough.strong_bias2, label='$Bias^2$', color='grey')  
-axs[1].plot(range(0, iter+1), models_rough.strong_variance, label='Variance', color='blue')  
+axs[1].plot(range(0, iter+1), models_rough.strong_error, color='orange', label='Error')
+axs[1].plot(range(0, iter+1), models_rough.strong_bias2, label='$Bias^2$', color='grey')
+axs[1].plot(range(0, iter+1), models_rough.strong_variance, label='Variance', color='blue')
 axs[1].axvline(x=rough_m, color='red', linestyle='--')
+axs[1].axvline(x=rough_strong_oracle, color='blue', linestyle=':')
 axs[1].set_xlabel('Iteration')
 axs[1].set_ylabel('Strong Quantities')
 
-axs[2].plot(range(0, iter+1), models_rough.weak_error, color='orange', label='Error')  
-axs[2].plot(range(0, iter+1), models_rough.weak_bias2, label='$Bias^2$', color='grey')  
-axs[2].plot(range(0, iter+1), models_rough.weak_variance, label='Variance', color='blue')  
+axs[2].plot(range(0, iter+1), models_rough.weak_error, color='orange', label='Error')
+axs[2].plot(range(0, iter+1), models_rough.weak_bias2, label='$Bias^2$', color='grey')
+axs[2].plot(range(0, iter+1), models_rough.weak_variance, label='Variance', color='blue')
 axs[2].axvline(x=rough_m, color='red', linestyle='--')
-axs[2].set_xlim([0, iter+1])  
-axs[2].set_ylim([0, 1])  
+axs[2].axvline(x=rough_weak_oracle, color='blue', linestyle=':')
+axs[2].set_xlim([0, 1200+1])
+axs[2].set_ylim([0, 1])
 axs[2].set_xlabel('Iteration')
 axs[2].set_ylabel('Weak Quantities')
 axs[2].legend()
@@ -135,6 +162,3 @@ plt.tight_layout()
 fig.suptitle('Rough Signal', fontsize=16)
 
 plt.show()
-
-
-#To Do: Give plots names: supersmooth, smooth, rough
--- src/EarlyStopping/landweber.py
+++ src/EarlyStopping/landweber.py
@@ -2,12 +2,13 @@ import numpy as np
 import scipy
 from scipy import sparse
 
+
 class Landweber:
     """ A class to perform estimation using the Landweber iterative method.
 
     Parameters
     ----------
-    input_matrix: array
+    design_matrix: array
         nxp design matrix of the linear model.
 
     response_variable: array
@@ -16,14 +17,14 @@ class Landweber:
     starting_value: array, default = None
         Determines the zeroth step of the iterative procedure. (Defaults to zero).
 
-    true_signal: array, default = None 
+    true_signal: array, default = None
         p-dim vector
         For simulation purposes only. For simulated data the true signal can be
         included to compute theoretical quantities such as the bias and the mse
         alongside the iterative procedure.
 
     true_noise_level: float, default = None
-        For simulation purposes only. Corresponds to the standard deviation 
+        For simulation purposes only. Corresponds to the standard deviation
         of normally distributed noise contributing to the response variable.
         Allows the analytic computation of the strong and weak variance.
 
@@ -31,7 +32,7 @@ class Landweber:
     ----------
     sample_size: int
         Sample size of the linear model
-    
+
     para_size: int
         Parameter size of the linear model
 
@@ -43,7 +44,7 @@ class Landweber:
 
     landweber_estimate: array
         Landweber estimate at the current iteration for the data given in
-        input_matrix
+        design_matrix
 
     residuals: array
         Lists the sequence of the squared residuals between the observed data and
@@ -54,26 +55,34 @@ class Landweber:
         bias up to the current Landweber iteration.
 
     strong_variance: array
-        Only exists if true_signal was given. Lists the values of the strong variance 
+        Only exists if true_signal was given. Lists the values of the strong variance
         up to the current Landweber iteration.
 
     strong_error: array
-        Only exists if true_signal was given. Lists the values of the strong norm error 
+        Only exists if true_signal was given. Lists the values of the strong norm error
         between the Landweber estimator and the true signal up to the
         current Landweber iteration.
-    
+
     weak_bias2: array
         Only exists if true_signal was given. Lists the values of the weak squared
         bias up to the current Landweber iteration.
 
     weak_variance: array
-        Only exists if true_signal was given. Lists the values of the weak variance 
+        Only exists if true_signal was given. Lists the values of the weak variance
         up to the current Landweber iteration.
 
     weak_error: array
-        Only exists if true_signal was given. Lists the values of the weak norm error 
+        Only exists if true_signal was given. Lists the values of the weak norm error
         between the Landweber estimator and the true signal up to the
         current Landweber iteration.
+
+    weak_balanced_oracle: integer
+        Only exists if true_signal was given. Gives the stopping iteration, when
+        weak_bias2 <= weak_variance.
+
+    strong_balanced_oracle: integer
+        Only exists if true_signal was given. Gives the stopping iteration, when
+        strong_bias2 <= strong_variance.
 
     Methods
     -------
@@ -82,31 +91,37 @@ class Landweber:
 
     landweber_to_early_stop(crit, max_iter)
         Applies early stopping to the Landweber iterative procedure.
-    """
-    def __init__(self, input_matrix,
-                 response_variable,
-                 learning_rate = 1,
-                 critical_value = None,
-                 starting_value = None,
-                 true_signal = None,
-                 true_noise_level = None):
 
-        #self.input_matrix = sparse.csr_matrix(input_matrix)
-        #self.response_variable = np.transpose(sparse.csr_matrix(response_variable))
-        self.input_matrix = input_matrix
+    landweber_gather_all(max_iter)
+        Runs the algorithm till max_iter is reached. The early_stopping_index is
+        recorded.
+
+    """
+
+    def __init__(self, design_matrix,
+                 response_variable,
+                 learning_rate=1,
+                 critical_value=None,
+                 starting_value=None,
+                 true_signal=None,
+                 true_noise_level=None):
+
+        # self.design_matrix = sparse.csr_matrix(design_matrix)
+        # self.response_variable = np.transpose(sparse.csr_matrix(response_variable))
+        self.design_matrix = design_matrix
         self.response_variable = response_variable
         self.learning_rate = learning_rate
         self.true_signal = true_signal
-        self.true_noise_level = true_noise_level #sigma
+        self.true_noise_level = true_noise_level  # sigma
         self.critical_value = critical_value
 
         # Parameters of the model
-        self.sample_size = np.shape(input_matrix)[0]
-        self.para_size   = np.shape(input_matrix)[1]
-        
+        self.sample_size = np.shape(design_matrix)[0]
+        self.para_size = np.shape(design_matrix)[1]
+
         # Determine starting value for the procedure
         if starting_value is None:
-            #self.starting_value = np.transpose(sparse.csr_matrix(np.zeros(self.para_size)))
+            # self.starting_value = np.transpose(sparse.csr_matrix(np.zeros(self.para_size)))
             self.starting_value = np.zeros(self.para_size)
         else:
             self.starting_value = starting_value
@@ -116,33 +131,35 @@ class Landweber:
         self.landweber_estimate = self.starting_value
         self.early_stopping_index = None
 
-        self.gram_matrix = np.transpose(self.input_matrix) @ self.input_matrix
-        
+        self.gram_matrix = np.transpose(self.design_matrix) @ self.design_matrix
+
         if self.critical_value is None and self.true_noise_level is None:
             # maximum likelihood estimator
-            # least_squares_estimator = np.linalg.solve(self.congruency_matrix, np.transpose(self.input_matrix) @ self.response_variable)
-            sparse_least_squares_estimator, istop = sparse.linalg.lsqr(self.gram_matrix, np.transpose(self.input_matrix) @ self.response_variable)[:2]
-            noise_level_estimate = np.sum((self.response_variable - self.input_matrix @ sparse_least_squares_estimator)**2)/self.sample_size
+            # least_squares_estimator = np.linalg.solve(self.congruency_matrix, np.transpose(self.design_matrix) @ self.response_variable)
+            sparse_least_squares_estimator, istop = sparse.linalg.lsqr(self.gram_matrix, np.transpose(
+                self.design_matrix) @ self.response_variable)[:2]
+            noise_level_estimate = np.sum(
+                (self.response_variable - self.design_matrix @ sparse_least_squares_estimator) ** 2) / self.sample_size
             self.critical_value = noise_level_estimate * self.sample_size
         elif self.true_noise_level is not None:
             # if the true noise level is given, it does not need to be estimated
-            self.critical_value = self.true_noise_level**2 * self.sample_size
+            self.critical_value = self.true_noise_level ** 2 * self.sample_size
 
         # Residual quantities
         self.__residual_vector = response_variable
-        self.residuals = np.array([np.sum(self.__residual_vector**2)])
+        self.residuals = np.array([np.sum(self.__residual_vector ** 2)])
 
-        if (self.true_signal is not None) and (self.true_noise_level is not None):            
+        if (self.true_signal is not None) and (self.true_noise_level is not None):
             # initialize matrices required for computing the strong/weak bias and variance
             self.inverse_congruency_matrix = np.linalg.inv(self.gram_matrix)
             self.perturbation_congruency_matrix = (np.eye(self.para_size) - self.learning_rate * self.gram_matrix)
-            self.weak_perturbation_congruency_matrix = (self.input_matrix @ self.perturbation_congruency_matrix)
+            self.weak_perturbation_congruency_matrix = (self.design_matrix @ self.perturbation_congruency_matrix)
             self.perturbation_congruency_matrix_power = self.perturbation_congruency_matrix
-            
+
             # initialize strong/weak bias and variance
             self.expectation_estimator = self.starting_value
-            self.strong_bias2 = np.array([np.sum(self.true_signal**2)])
-            self.weak_bias2 = np.array([np.sum((self.input_matrix @ self.true_signal)**2)])
+            self.strong_bias2 = np.array([np.sum(self.true_signal ** 2)])
+            self.weak_bias2 = np.array([np.sum((self.design_matrix @ self.true_signal) ** 2)])
 
             self.strong_variance = np.array([0])
             self.weak_variance = np.array([0])
@@ -150,9 +167,12 @@ class Landweber:
             self.strong_error = self.strong_bias2 + self.strong_variance
             self.weak_error = self.weak_bias2 + self.weak_variance
 
-    def landweber(self, iter_num = 1):
+            self.weak_balanced_oracle = 0
+            self.strong_balanced_oracle = 0
+
+    def landweber(self, iter_num=1):
         """Performs iter_num iterations of the Landweber algorithm
-        
+
         Parameters
         ----------
         iter_num: int, default=1
@@ -163,71 +183,73 @@ class Landweber:
 
     def __update_iterative_matrices(self):
         """Update iterative quantities
-        
+
         - The expectation of the estimator satisfies
-        :math: `m_{k+1} = m_k + hA^{\\top}A(\\mu-m_k)` 
-        - Compute matrix power iteratively 
+        :math: `m_{k+1} = m_k + hA^{\\top}A(\\mu-m_k)`
+        - Compute matrix power iteratively
         :math: `(I-hA^{\\top}A)^{k+1}=(I-hA^{\\top}A)^{k} * (I-hA^{\\top}A)`
         """
         self.expectation_estimator = (self.expectation_estimator
-            + self.learning_rate * (self.gram_matrix)
-            @ (self.true_signal - self.expectation_estimator))
-        
+                                      + self.learning_rate * (self.gram_matrix)
+                                      @ (self.true_signal - self.expectation_estimator))
+
         self.perturbation_congruency_matrix_power = (self.perturbation_congruency_matrix_power
-            @ self.perturbation_congruency_matrix)
+                                                     @ self.perturbation_congruency_matrix)
 
     def __update_strong_bias2(self):
         """Update strong bias
 
-        Given the expectation of the estimator the squared bias is given by :math: `b_k^{2} = \\Vert (I-hA^{\\top}A)(\\mu-m_{k-1}) \\Vert^{2}` 
+        Given the expectation of the estimator the squared bias is given by :math: `b_k^{2} = \\Vert (I-hA^{\\top}A)(\\mu-m_{k-1}) \\Vert^{2}`
         """
         new_strong_bias2 = np.sum((self.perturbation_congruency_matrix
-            @ (self.true_signal - self.expectation_estimator))**2)
+                                   @ (self.true_signal - self.expectation_estimator)) ** 2)
         self.strong_bias2 = np.append(self.strong_bias2, new_strong_bias2)
 
     def __update_weak_bias2(self):
         """Update weak bias
 
-        Given the expectation of the estimator the (weak)-squared bias is given by :math: `b_k^{2} = \\Vert A(I-hA^{\\top}A)(\\mu-m_{k-1}) \\Vert^{2}` 
+        Given the expectation of the estimator the (weak)-squared bias is given by :math: `b_k^{2} = \\Vert A(I-hA^{\\top}A)(\\mu-m_{k-1}) \\Vert^{2}`
         """
         new_weak_bias2 = np.sum((self.weak_perturbation_congruency_matrix
-            @ (self.true_signal - self.expectation_estimator))**2)
+                                 @ (self.true_signal - self.expectation_estimator)) ** 2)
         self.weak_bias2 = np.append(self.weak_bias2, new_weak_bias2)
 
     def __update_strong_variance(self):
-        """update strong variance
-        
-        The strong variance in the m-th iteration is given by 
+        """Update strong variance
+
+        The strong variance in the m-th iteration is given by
         :math: `\\sigma**2 \\mathrm{tr}(h^{-1}(A^{\\top}A)^{-1}(I-(I-hA^{\\top}A)^{m}))`
         """
-        new_strong_variance = (self.true_noise_level**2 *
-            np.trace(self.learning_rate**(-1) * self.inverse_congruency_matrix
-            @ np.square(np.eye(self.para_size) - self.perturbation_congruency_matrix_power)))
+        new_strong_variance = (self.true_noise_level ** 2 *
+                               np.trace(self.learning_rate ** (-1) * self.inverse_congruency_matrix
+                                        @ np.square(
+                                   np.eye(self.para_size) - self.perturbation_congruency_matrix_power)))
         self.strong_variance = np.append(self.strong_variance, new_strong_variance)
-        
+
     def __update_weak_variance(self):
-        """update weak variance
-        The weak variance in the m-th iteration is given by 
+        """Update weak variance
+
+        The weak variance in the m-th iteration is given by
         :math: `\\sigma**2 \\mathrm{tr}((I-(I-hA^{\\top}A)^{m}))`
         """
-        new_weak_variance = (self.true_noise_level**2 *
-            np.trace(np.square(np.eye(self.para_size) - self.perturbation_congruency_matrix_power)))
+        new_weak_variance = (self.true_noise_level ** 2 *
+                             np.trace(np.square(np.eye(self.para_size) - self.perturbation_congruency_matrix_power)))
         self.weak_variance = np.append(self.weak_variance, new_weak_variance)
 
     def __landweber_one_iteration(self):
         """Performs one iteration of the Landweber algorithm"""
         self.landweber_estimate = (self.landweber_estimate
-            + self.learning_rate * np.transpose(self.input_matrix)
-            @ (self.response_variable - self.input_matrix @ self.landweber_estimate))
+                                   + self.learning_rate * np.transpose(self.design_matrix)
+                                   @ (self.response_variable - self.design_matrix @ self.landweber_estimate))
 
         # self.landweber_estimate = (self.landweber_estimate
-        #     + self.learning_rate * np.transpose(self.input_matrix)
-        #     * (self.response_variable - self.input_matrix * self.landweber_estimate))
+        #     + self.learning_rate * np.transpose(self.design_matrix)
+        #     * (self.response_variable - self.design_matrix * self.landweber_estimate))
 
         # Update estimation quantities
-        self.__residual_vector  = (self.response_variable
-            - np.matmul(self.input_matrix,self.landweber_estimate))
-        new_residuals = np.sum(self.__residual_vector**2)
+        self.__residual_vector = (self.response_variable
+                                  - np.matmul(self.design_matrix, self.landweber_estimate))
+        new_residuals = np.sum(self.__residual_vector ** 2)
         self.residuals = np.append(self.residuals, new_residuals)
         self.iter = self.iter + 1
 
@@ -262,7 +284,19 @@ class Landweber:
         self.early_stopping_index = self.iter
 
     def landweber_gather_all(self, max_iter):
-        """Gather all relevant simulation data (Runs the algorithm till max_iter) but tracks the early stopping index."""
+        """Runs the algorithm till max_iter and gathers all relevant simulation data.
+        The early stopping index is recorded.
+
+        Parameters
+        ----------
+        max_iter: int
+            The maximum number of iterations to perform.
+        """
         self.landweber_to_early_stop(max_iter)
         if max_iter > self.early_stopping_index:
             self.landweber(max_iter - self.early_stopping_index)
+        if (self.true_signal is not None) and (self.true_noise_level is not None):
+            self.weak_balanced_oracle = (np.argmax(self.weak_bias2 <= self.weak_variance)) if np.any(
+                self.weak_bias2 <= self.weak_variance) else None
+            self.strong_balanced_oracle = (np.argmax(self.strong_bias2 <= self.strong_variance)) if np.any(
+                self.strong_bias2 <= self.strong_variance) else None
