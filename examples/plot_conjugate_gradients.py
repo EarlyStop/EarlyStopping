4bb1ddf43fb6e2114cf7712b7fbb1bece36ad872
Laura Hucker <huckerla@math.hu-berlin.de>
2024-01-19 16:59:58 +0100

    Added plot of interpolated squared residual norms

--- examples/plot_conjugate_gradients.py
+++ examples/plot_conjugate_gradients.py
@@ -20,7 +20,7 @@ random.seed(42)
 # To make our results comparable, we use the same simulation setting as `Blanchard et al. (2018) <https://doi.org/10.1137/17M1154096>`_ and `Stankewitz (2020) <https://doi.org/10.1214/20-EJS1747>`_.
 
 # Set parameters
-SAMPLE_SIZE = 10000  # set to 10000 for final simulations
+SAMPLE_SIZE = 10000
 PARAMETER_SIZE = SAMPLE_SIZE
 MAXIMAL_ITERATION = SAMPLE_SIZE
 NOISE_LEVEL = 0.01
@@ -53,7 +53,7 @@ plt.show()
 # We simulate NUMBER_RUNS realisations of the Gaussian sequence space model.
 
 # Specify number of Monte Carlo runs
-NUMBER_RUNS = 10  # set to 1000 for final simulations
+NUMBER_RUNS = 1  # set to 1000 for final simulations
 
 # Create observations for the three different signals
 noise = np.random.normal(0, NOISE_LEVEL, (SAMPLE_SIZE, NUMBER_RUNS))
@@ -112,3 +112,49 @@ for run in range(NUMBER_RUNS):
     print(f"Supersmooth early stopping index: {models_supersmooth[run].early_stopping_index}")
     print(f"Smooth early stopping index: {models_smooth[run].early_stopping_index}")
     print(f"Rough early stopping index: {models_rough[run].early_stopping_index}")
+
+# %%
+# Plot of squared residual norms and empirical error terms
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# We plot for the first Monte Carlo run the squared residual norms along the whole iteration path and the corresponding weak and strong empirical error terms,
+# i.e. the empirical prediction and reconstruction errors, for the three different signals. If we chose to interpolate, i.e. INTERPOLSTION_BOOLEAN is set to
+# true, we need to interpolate between the squared residual norms at the integer iteration indices.
+
+
+# Calculate interpolated squared residual norm for a given vector containing the squared
+# residual norms at integer iteration indices and a possibliy non-integer index
+def calculate_interpolated_residual(residuals, index):
+    index_ceil = int(np.ceil(index))
+    index_floor = int(np.floor(index))
+    alpha = index - index_floor
+    interpolated_residual = (1 - alpha) ** 2 * residuals[index_floor] + (1 - (1 - alpha) ** 2) * residuals[index_ceil]
+    return interpolated_residual
+
+
+# Vectorize interpolating function
+calculate_interpolated_residual = np.vectorize(calculate_interpolated_residual, excluded=["residuals"])
+
+# Set gridsize for the x-axis of the plots
+GRIDSIZE = 0.01
+
+# Calculate interpolated squared residual norms
+if INTERPOLATION_BOOLEAN:
+    grid = np.arange(0, MAXIMAL_ITERATION + GRIDSIZE, GRIDSIZE)
+    residuals_supersmooth = calculate_interpolated_residual(residuals=models_supersmooth[0].residuals, index=grid)
+else:
+    grid = np.arange(0, MAXIMAL_ITERATION + 1)
+    residuals_supersmooth = models_supersmooth[0].residuals
+
+# Plot
+plt.plot(grid, residuals_supersmooth, label="squared residual norm", color="green")
+plt.axvline(x=models_supersmooth[0].early_stopping_index, color="grey", linestyle="--")
+plt.axhline(y=models_supersmooth[0].critical_value, color="grey", linestyle="--")
+plt.xlim([0, 12])
+plt.ylim([0, 11])
+plt.xlabel("Iteration index")
+plt.xticks(list(plt.xticks()[0]) + [models_supersmooth[0].early_stopping_index], list(plt.xticks()[1]) + ["$\\tau$"])
+plt.yticks(
+    list(plt.yticks()[0]) + [models_supersmooth[0].critical_value], list(plt.yticks()[1]) + ["$\\kappa = D \\delta^2$"]
+)
+plt.legend()
+plt.show()
