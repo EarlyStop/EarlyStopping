245deeb75613fc77c20e06cd7846d0bb1f0c8ab6
Ratmir Miftachov <40966507+ratmir-miftachov@users.noreply.github.com>
2024-03-11 18:41:09 +0100

    experiments on sparse and dense matrix

--- src/EarlyStopping/landweber.py
+++ src/EarlyStopping/landweber.py
@@ -135,6 +135,7 @@ class Landweber:
             if (scipy.sparse.issparse(self.gram_matrix)):
                self.inverse_congruency_matrix = scipy.sparse.linalg.inv(self.gram_matrix)
             else:
+                print("hello")
                 self.inverse_congruency_matrix = np.linalg.inv(self.gram_matrix)
 
             self.perturbation_congruency_matrix = (sparse.dia_matrix(np.eye(self.para_size)) - self.learning_rate * self.gram_matrix)
