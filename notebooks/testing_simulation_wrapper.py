73f0bb813a689d15a0a01435c840cdc6d9af143a
ziebelle <32039090+ziebelle@users.noreply.github.com>
2024-06-07 18:14:23 +0200

    Update of the simmulation wrapper
    Green text indicating the progress of the algorithm
    Error based choice of the learning rate for landweber
    Add weak errors

--- notebooks/testing_simulation_wrapper.py
+++ notebooks/testing_simulation_wrapper.py
@@ -1,7 +1,6 @@
 for name in list(globals()):
-    if not name.startswith('_'):
+    if not name.startswith("_"):
         del globals()[name]
-
 
 
 import numpy as np
@@ -11,18 +10,35 @@ from scipy.sparse import dia_matrix
 
 importlib.reload(es)
 
-sample_size = 1000
-indices = np.arange(1000) + 1
+# sample_size = 1000
+# indices = np.arange(sample_size) + 1
 
-parameters = es.SimulationParameters(
-    design=dia_matrix(np.diag(1 / np.sqrt(indices))),
-    true_signal=5 * np.exp(-0.1 * indices),
+# parameters_supersmooth = es.SimulationParameters(
+#     design=dia_matrix(np.diag(1 / np.sqrt(indices))),
+#     true_signal=5 * np.exp(-0.1 * indices),
+#     true_noise_level=0.01,
+#     max_iterations=1000,
+# )
+
+sample_size_gravity = 100  # 2**9
+a = 0
+b = 1
+d = 0.25  # Parameter controlling the ill-posedness: the larger, the more ill-posed, default in regtools: d = 0.25
+
+t = (np.arange(1, sample_size_gravity + 1) - 0.5) / sample_size_gravity
+s = ((np.arange(1, sample_size_gravity + 1) - 0.5) * (b - a)) / sample_size_gravity
+T, S = np.meshgrid(t, s)
+
+
+parameters_gravity = es.SimulationParameters(
+    design=(1 / sample_size_gravity)
+    * d
+    * (d**2 * np.ones((sample_size_gravity, sample_size_gravity)) + (S - T) ** 2) ** (-(3 / 2)),
+    true_signal=np.sin(np.pi * t) + 0.5 * np.sin(2 * np.pi * t),
     true_noise_level=0.01,
-    max_iterations=1000)
+    max_iterations=1000,
+)
 
-simulation = es.SimulationWrapper(**parameters.__dict__)
+simulation = es.SimulationWrapper(**parameters_gravity.__dict__)
 
 results = simulation.run_simulation()
-
-
-
--- src/EarlyStopping/simulation_wrapper.py
+++ src/EarlyStopping/simulation_wrapper.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import pandas as pd
 from scipy.sparse.linalg import svds
 
+
 class SimulationParameters:
     def __init__(self, design, true_signal, true_noise_level, max_iterations):
         self.design = design
@@ -23,14 +24,11 @@ class SimulationParameters:
         if not isinstance(self.max_iterations, int) or self.max_iterations <= 0:
             raise ValueError("max_iterations must be a positive integer")
 
+
 class SimulationWrapper:
-    def __init__(self,
-                 design,
-                 true_signal=None,
-                 true_noise_level=None,
-                 monte_carlo_runs=5,
-                 max_iterations=1000,
-                 cores=3):
+    def __init__(
+        self, design, true_signal=None, true_noise_level=None, monte_carlo_runs=5, max_iterations=1000, cores=3
+    ):
         self.design = design
         self.true_signal = true_signal
         self.true_noise_level = true_noise_level
@@ -40,82 +38,133 @@ class SimulationWrapper:
 
         self.sample_size = design.shape[0]
 
-
     def run_simulation(self):
+        info("Running simulation.")
         noise = np.random.normal(0, self.true_noise_level, (self.sample_size, self.monte_carlo_runs))
         self.response = noise + (self.design @ self.true_signal)[:, None]
-        self.learning_rate = self.__search_learning_rate(search_depth=3)
-        self.results = np.vstack(Parallel(n_jobs=self.cores)(delayed(self.monte_carlo_wrapper)(m) for m in range(self.monte_carlo_runs)))
-        self.__visualise()
+
+        info("Searching for viable learning rates.")
+        self.learning_rate = self.__search_learning_rate(search_depth=10)
+
+        info("Running Monte Carlo simulation.")
+        self.results = Parallel(n_jobs=self.cores)(
+            delayed(self.monte_carlo_wrapper)(m) for m in range(self.monte_carlo_runs)
+        )
+
+        strong_errors, weak_errors = zip(*self.results)
+
+        self.__visualise(np.vstack(strong_errors), "Strong Empirical Error at $\\tau$")
+        self.__visualise(np.vstack(weak_errors), "Weak Empirical Error at $\\tau$")
         return self.results
-    
-    def search_learning_rate_wrapper(self, m):
-        model_landweber = Landweber(
-            design=self.design, response=self.response[:, 0], true_signal=self.true_signal, true_noise_level=self.true_noise_level, learning_rate=self.learning_rate_candidates[m])
-        
-        model_landweber.iterate(self.max_iterations)
-
-        converges = True
-        for m in range((self.max_iterations - 1)):
-            if model_landweber.residuals[m] < model_landweber.residuals[(m + 1)]:
-                converges = False
-
-        return converges
 
     def __search_learning_rate(self, search_depth):
         u, s, vh = svds(self.design, k=1)
         largest_singular_value = s[0]
         initial_guess_learning_rate = 2 / largest_singular_value**2
-        self.learning_rate_candidates = np.array([initial_guess_learning_rate /(2**i) for i in range(search_depth)])
-        print(self.learning_rate_candidates)
-        learning_rate_candidates_evaluation = np.vstack(Parallel(n_jobs=self.cores)(delayed(self.search_learning_rate_wrapper)(m) for m in range(search_depth)))
-        print(np.array(learning_rate_candidates_evaluation.flatten()))
-        accepted_candidates = self.learning_rate_candidates[np.array(learning_rate_candidates_evaluation.flatten())]
-        return np.max(accepted_candidates)
+        self.learning_rate_candidates = np.array([initial_guess_learning_rate / (2**i) for i in range(search_depth)])
+        info("Determine learning rate candidates based on search depth.")
 
+        results = Parallel(n_jobs=self.cores)(
+            delayed(self.search_learning_rate_wrapper)(m) for m in range(search_depth)
+        )
+
+        learning_rate_candidates_evaluation, strong_errors = zip(*results)
+
+        true_false_vector = np.array(np.vstack(learning_rate_candidates_evaluation).flatten())
+        accepted_candidates = self.learning_rate_candidates[true_false_vector]
+        accepted_errors = np.array(strong_errors)[true_false_vector]
+        return accepted_candidates[np.argmin(accepted_errors)]
+
+    def search_learning_rate_wrapper(self, m):
+        model_landweber = Landweber(
+            design=self.design,
+            response=self.response[:, 0],
+            true_signal=self.true_signal,
+            true_noise_level=self.true_noise_level,
+            learning_rate=self.learning_rate_candidates[m],
+        )
+
+        model_landweber.iterate(self.max_iterations)
+
+        converges = True
+        for k in range((self.max_iterations - 1)):
+            if model_landweber.residuals[k] < model_landweber.residuals[(k + 1)]:
+                converges = False
+
+        if converges is False:
+            info(f"The estimator does not converge for learning rate: {self.learning_rate_candidates[m]}", color="red")
+
+        return converges, model_landweber.strong_empirical_error[model_landweber.get_early_stopping_index()]
 
     def monte_carlo_wrapper(self, m):
+        info(f"Monte Carlo run {m + 1}/{self.monte_carlo_runs}.")
         model_landweber = Landweber(
-            design=self.design, response=self.response[:, m], true_signal=self.true_signal, true_noise_level=self.true_noise_level, learning_rate=self.learning_rate)
+            design=self.design,
+            response=self.response[:, m],
+            true_signal=self.true_signal,
+            true_noise_level=self.true_noise_level,
+            learning_rate=self.learning_rate,
+        )
         model_conjugate_gradients = ConjugateGradients(
-            design=self.design, response=self.response[:, m], true_signal=self.true_signal, true_noise_level=self.true_noise_level)
+            design=self.design,
+            response=self.response[:, m],
+            true_signal=self.true_signal,
+            true_noise_level=self.true_noise_level,
+        )
 
         model_conjugate_gradients.gather_all(self.max_iterations)
         model_landweber.iterate(self.max_iterations)
 
-        landweber_strong_empirical_error = model_landweber.strong_empirical_error[model_landweber.get_early_stopping_index()]
-        conjugate_gradients_strong_empirical_error = model_conjugate_gradients.strong_empirical_errors[model_conjugate_gradients.early_stopping_index]
+        landweber_strong_empirical_error = model_landweber.strong_empirical_error[
+            model_landweber.get_early_stopping_index()
+        ]
+        conjugate_gradients_strong_empirical_error = model_conjugate_gradients.strong_empirical_errors[
+            model_conjugate_gradients.early_stopping_index
+        ]
 
-        return np.array([landweber_strong_empirical_error, conjugate_gradients_strong_empirical_error])
+        landweber_weak_empirical_error = model_landweber.weak_empirical_error[
+            model_landweber.get_early_stopping_index()
+        ]
+        conjugate_gradients_weak_empirical_error = model_conjugate_gradients.weak_empirical_errors[
+            model_conjugate_gradients.early_stopping_index
+        ]
 
-    def __visualise(self):
-        strong_empirical_errors_Monte_Carlo = pd.DataFrame(
-            {
-                "conjugate_gradient": self.results[:, 1],
-                "landweber": self.results[:, 0]
-            }
+        return (
+            np.array([landweber_strong_empirical_error, conjugate_gradients_strong_empirical_error]),
+            np.array([landweber_weak_empirical_error, conjugate_gradients_weak_empirical_error]),
         )
 
-        strong_empirical_errors_Monte_Carlo = pd.melt(
-            strong_empirical_errors_Monte_Carlo,
+    def __visualise(self, quantity_to_visualise, quantity_name):
+
+        comparison_table_quantity = pd.DataFrame(
+            {"conjugate_gradient": quantity_to_visualise[:, 1], "landweber": quantity_to_visualise[:, 0]}
+        )
+
+        comparison_table_quantity = pd.melt(
+            comparison_table_quantity,
             value_vars=["conjugate_gradient", "landweber"],
         )
 
         plt.figure(figsize=(14, 10))
-        strong_empirical_errors_boxplot = sns.boxplot(
+        quantity_boxplot = sns.boxplot(
             x="variable",
             y="value",
-            data=strong_empirical_errors_Monte_Carlo,
+            data=comparison_table_quantity,
             width=0.8,
             palette=["tab:purple", "tab:purple"],
         )
-        strong_empirical_errors_boxplot.set_ylabel("Strong Empirical Error at $\\tau$",
-                                                   fontsize=24)  # Increase fontsize
-        strong_empirical_errors_boxplot.set_xlabel("Data generating processes", fontsize=24)  # Increase fontsize
-        strong_empirical_errors_boxplot.set_xticklabels(strong_empirical_errors_boxplot.get_xticklabels(), rotation=45)
+        quantity_boxplot.set_ylabel(f"{quantity_name}", fontsize=24)  # Increase fontsize
+        quantity_boxplot.set_xlabel("Data generating processes", fontsize=24)  # Increase fontsize
+        quantity_boxplot.set_xticklabels(quantity_boxplot.get_xticklabels(), rotation=45)
 
-        strong_empirical_errors_boxplot.tick_params(axis="both", which="major", labelsize=24)  # Increase fontsize
-        plt.title("Comparison of Strong Empirical Errors", fontsize=28)  # Increase title fontsize
+        quantity_boxplot.tick_params(axis="both", which="major", labelsize=24)  # Increase fontsize
+        plt.title(f"Comparison of {quantity_name}", fontsize=28)  # Increase title fontsize
         plt.tight_layout()
         plt.show()
 
+
+def info(message, color="green"):
+    if color == "green":
+        print(f"\033[92m{message}\033[0m")
+    if color == "red":
+        print(f"\033[31m{message}\033[0m")
