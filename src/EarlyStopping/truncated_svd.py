dd6c1e705784745c8d6fc1b4a1464f481d7446fd
ziebelle <ziebelle@users.noreply.github.com>
2024-06-18 16:00:50 +0200

    Update truncated svds

--- src/EarlyStopping/truncated_svd.py
+++ src/EarlyStopping/truncated_svd.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 from scipy import sparse
+from scipy.sparse.linalg import svds
 
 class TruncatedSVD:
     def __init__(self,
@@ -11,23 +12,39 @@ class TruncatedSVD:
         true_noise_level=None,
     ):
         self.design = design
+        self.reduced_design = design
         self.response = response
         self.critical_value = critical_value
         self.true_signal = true_signal
         self.true_noise_level = true_noise_level
 
         # TODO: preprocessing so that SVD is applied for preprocessing
-        self.diagonal_design = self.design
-        self.sample_size = self.diagonal_design.size
-        self.diagonal_response = self.response
+        self.diagonal_design = np.array([])
+        self.sample_size = np.shape(self.design)[0]
+        self.parameter_size = np.shape(self.design)[1]
+        self.diagonal_response = np.array([])
+        self.diagonal_estimate = np.array([])
+        self.eigenvector_matrix = np.empty((self.parameter_size, 0))
 
         self.iteration = 0
-        self.truncated_svd_estimate = np.zeros(self.sample_size)
+        self.truncated_svd_estimate = np.zeros(self.parameter_size)
+        
 
     def iterate(self, number_of_iterations):
         for _ in range(number_of_iterations):
             self.__truncated_SVD_one_iteration()
     
     def __truncated_SVD_one_iteration(self):
-        self.truncated_svd_estimate[self.iteration] = self.diagonal_response[self.iteration] / self.diagonal_design[self.iteration]
+        u, s, vh = svds(self.reduced_design, k=1)
+        print(f"s: {s}")
+
+        self.diagonal_design = np.append(self.diagonal_design, s)
+        self.diagonal_response = np.append(self.diagonal_response, vh @ self.response)
+        self.eigenvector_matrix = np.append(self.eigenvector_matrix, vh.transpose(), axis=1)
+
+        self.reduced_design = self.reduced_design - s * u @ vh
+
+        self.diagonal_estimate = np.append(self.diagonal_estimate, self.diagonal_response[self.iteration] / s)
+        self.truncated_svd_estimate += self.diagonal_estimate[self.iteration] * vh.flatten()
+        # self.truncated_svd_estimate[self.iteration] = self.diagonal_response[self.iteration] / self.diagonal_design[self.iteration]
         self.iteration += 1
--- tests/test_truncated_svd.py
+++ tests/test_truncated_svd.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import EarlyStopping as es
+from scipy.sparse.linalg import svds
 
 class TestTruncatedSVD(unittest.TestCase):
     def setUp(self):
@@ -8,15 +9,19 @@ class TestTruncatedSVD(unittest.TestCase):
         # Simulate data
         self.sample_size = 5
         self.para_size = 5
-        self.design = np.random.normal(0, 1, size = self.sample_size)
+        self.design = np.diag(np.random.normal(0, 1, size = self.sample_size))
+        u, s, vh = svds(self.design, k=4)
+        print(s)
         self.signal = np.random.uniform(0, 1, size = self.sample_size)
         self.noise = np.random.normal(0, 5, self.sample_size)
-        self.noiseless_response = self.design * self.signal
+        self.noiseless_response = self.design @ self.signal
         self.response = self.noiseless_response + self.noise
 
     def test_inversion_without_noise(self):
         self.alg = es.TruncatedSVD(self.design, self.noiseless_response)
         self.alg.iterate(self.sample_size)
+        print(f"signal {self.signal}")
+        print(f"estimate {self.alg.truncated_svd_estimate}")
         self.assertAlmostEqual(np.mean(self.alg.truncated_svd_estimate - self.signal), 0, places=5)
 
         
