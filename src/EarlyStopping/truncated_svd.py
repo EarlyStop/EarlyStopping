8aa205d21254de95ae228b352be10cdf83d1f070
be5tan <bernhard.stankewitz@posteo.de>
2024-07-17 11:07:14 +0200

    Added residuals w/ consistency check.

--- src/EarlyStopping/truncated_svd.py
+++ src/EarlyStopping/truncated_svd.py
@@ -11,22 +11,25 @@ class TruncatedSVD:
         true_signal=None,
         true_noise_level=None,
     ):
+
         self.design = design
-        self.reduced_design = design
         self.response = response
         self.critical_value = critical_value
         self.true_signal = true_signal
         self.true_noise_level = true_noise_level
 
-        # TODO: preprocessing so that SVD is applied for preprocessing
-        self.diagonal_design = np.array([])
         self.sample_size = np.shape(self.design)[0]
         self.parameter_size = np.shape(self.design)[1]
+
+        self.diagonal_design = np.array([])
         self.diagonal_response = np.array([])
         self.diagonal_estimate = np.array([])
+
+        self.reduced_design = design
         self.eigenvector_matrix = np.empty((self.parameter_size, 0))
 
         self.iteration = 0
+        self.residuals = np.array([np.sum(self.response**2)])
         self.truncated_svd_estimate = np.zeros(self.parameter_size)
         
 
@@ -35,16 +38,22 @@ class TruncatedSVD:
             self.__truncated_SVD_one_iteration()
     
     def __truncated_SVD_one_iteration(self):
+        # Get next singular triplet
         u, s, vh = svds(self.reduced_design, k=1)
-        print(f"s: {s}")
 
+        # Get diagonal sequence model quantities
         self.diagonal_design = np.append(self.diagonal_design, s)
         self.diagonal_response = np.append(self.diagonal_response, u.transpose() @ self.response)
         self.eigenvector_matrix = np.append(self.eigenvector_matrix, vh.transpose(), axis=1)
+        self.diagonal_estimate = np.append(self.diagonal_estimate,
+                                           self.diagonal_response[self.iteration] / s)
 
-        self.reduced_design = self.reduced_design - s * u @ vh
-
-        self.diagonal_estimate = np.append(self.diagonal_estimate, self.diagonal_response[self.iteration] / s)
+        # Update full model quantities
         self.truncated_svd_estimate += self.diagonal_estimate[self.iteration] * vh.flatten()
-        # self.truncated_svd_estimate[self.iteration] = self.diagonal_response[self.iteration] / self.diagonal_design[self.iteration]
+        new_residual = self.residuals[self.iteration] - (u.transpose() @ self.response)**2
+        self.residuals = np.append(self.residuals, new_residual)
+
+        # Reduce design by one eigen triplet 
+        self.reduced_design = self.reduced_design - s * u @ vh
+        
         self.iteration += 1
--- tests/test_truncated_svd.py
+++ tests/test_truncated_svd.py
@@ -27,15 +27,15 @@ class TestTruncatedSVD(unittest.TestCase):
         alg.iterate(self.sample_size)
         self.assertAlmostEqual(np.mean(alg.truncated_svd_estimate - signal), 0, places=5)
 
-    # def test_inversion_without_noise(self):
-    #     design = np.diag(np.random.normal(0, 1, size = self.sample_size))
-    #     signal = np.random.uniform(0, 1, size = self.sample_size)
-    #     noise = np.random.normal(0, 1, self.sample_size)
-    #     self.noiseless_response = self.design @ self.signal
-    #     self.response = self.noiseless_response + self.noise
-    #     self.alg = es.TruncatedSVD(self.design, self.noiseless_response)
-    #     self.alg.iterate(self.sample_size)
-    #     self.assertAlmostEqual(np.mean(self.alg.truncated_svd_estimate - self.signal), 0, places=5)
+    def test_residuals_go_to_zero(self):
+        design = np.random.normal(0, 1, 
+                                  size = (self.sample_size, self.sample_size))
+        signal = np.random.uniform(0, 1, size = self.sample_size)
+        noiseless_response = design @ signal
+        alg = es.TruncatedSVD(design, noiseless_response)
+        alg.iterate(self.sample_size)
+        self.assertAlmostEqual(alg.residuals[alg.iteration], 0, places=5)
+
 
         
 if __name__ == '__main__':
