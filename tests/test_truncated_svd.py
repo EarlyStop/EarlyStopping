a6c8501b6f63f7c7c16235a619c9f354c8955d7b
be5tan <bernhard.stankewitz@posteo.de>
2024-09-04 15:28:57 +0200

    Finished monotonicity test of theoretical quantities.

--- tests/test_truncated_svd.py
+++ tests/test_truncated_svd.py
@@ -35,18 +35,28 @@ class TestTruncatedSVD(unittest.TestCase):
 
         for _ in range(self.sample_size):
             alg.iterate(1)
-            self.assertLessEqual(alg.weak_bias2[alg.iteration], alg.weak_bias2[alg.iteration])
 
-    # def test_residuals_go_to_zero(self):
-    #     design = np.random.normal(0, 1, 
-    #                               size = (self.sample_size, self.sample_size))
-    #     signal = np.random.uniform(0, 1, size = self.sample_size)
-    #     noiseless_response = design @ signal
-    #     alg = es.TruncatedSVD(design, noiseless_response)
-    #     alg.iterate(self.sample_size)
-    #     self.assertAlmostEqual(alg.residuals[alg.iteration], 0, places=5)
+            self.assertLessEqual(alg.weak_bias2[alg.iteration], alg.weak_bias2[alg.iteration - 1])
+            self.assertLessEqual(alg.strong_bias2[alg.iteration], alg.strong_bias2[alg.iteration - 1])
+
+            self.assertLessEqual(alg.weak_variance[alg.iteration - 1], alg.weak_variance[alg.iteration])
+            self.assertLessEqual(alg.strong_variance[alg.iteration - 1], alg.strong_variance[alg.iteration])
+
+    def test_diagonal_monotonicity_of_theoretical_quantities(self):
+        design = np.diag(np.random.uniform(0, 1, size = self.sample_size))
+        signal = np.random.uniform(0, 1, size = self.sample_size)
+        response = design @ signal + np.random.normal(0, 0.1, self.sample_size)
+        alg = es.TruncatedSVD(design, response, true_signal = signal, true_noise_level = 0.1, diagonal = True)
+
+        for _ in range(self.sample_size):
+            alg.iterate(1)
+
+            self.assertLessEqual(alg.weak_bias2[alg.iteration], alg.weak_bias2[alg.iteration - 1])
+            self.assertLessEqual(alg.strong_bias2[alg.iteration], alg.strong_bias2[alg.iteration - 1])
+
+            self.assertLessEqual(alg.weak_variance[alg.iteration - 1], alg.weak_variance[alg.iteration])
+            self.assertLessEqual(alg.strong_variance[alg.iteration - 1], alg.strong_variance[alg.iteration])
 
 
-        
 if __name__ == '__main__':
      unittest.main()
