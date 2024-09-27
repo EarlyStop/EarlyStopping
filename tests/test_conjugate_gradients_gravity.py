da3fad28c406c982d91ebb0b6d28ab1c9d696b85
Laura Hucker <huckerla@math.hu-berlin.de>
2024-09-27 14:21:14 +0200

    Updated tests for new structure of conjugate_gradients.py

--- tests/test_conjugate_gradients_gravity.py
+++ tests/test_conjugate_gradients_gravity.py
@@ -32,7 +32,6 @@ class TestConjugateGradientsGravity(unittest.TestCase):
         self.parameter_size = self.sample_size
         self.max_iter = self.sample_size
         self.noise_level = 10 ** (-2)
-        self.critical_value = self.sample_size * (self.noise_level**2)
 
         # Specify number of Monte Carlo runs
         self.NUMBER_RUNS = 100
@@ -48,132 +47,104 @@ class TestConjugateGradientsGravity(unittest.TestCase):
             self.design @ self.signal,
             true_signal=self.signal,
             true_noise_level=0,
-            interpolation=False,
             computation_threshold=0,
         )
         model.iterate(2 * self.sample_size)
-        self.assertAlmostEqual(sum((model.conjugate_gradient_estimate - self.signal) ** 2), 0, places=5)
+        self.assertAlmostEqual(sum((model.get_estimate(model.iteration) - self.signal) ** 2), 0, places=5)
 
     def calculate_residual(self, response, design, conjugate_gradient_estimate):
         return np.sum((response - design @ conjugate_gradient_estimate) ** 2)
 
     def test_residuals(self):
         # Test if the entry in the residuals vector at the discrepancy stopping index agrees with the squared residual of the conjugate gradient estimate at the same index
+        critical_value = (np.sqrt(self.sample_size) + self.sample_size) * (self.noise_level**2)
         models = [
             es.ConjugateGradients(
                 self.design,
                 self.observation[:, i],
                 true_signal=self.signal,
                 true_noise_level=self.noise_level,
-                interpolation=False,
                 computation_threshold=0,
             )
             for i in range(self.NUMBER_RUNS)
         ]
 
+        max_iteration = self.sample_size
         for run in range(self.NUMBER_RUNS):
-            models[run].discrepancy_stop(self.max_iter)
+            early_stopping_index = models[run].get_discrepancy_stop(critical_value, max_iteration)
+            if early_stopping_index is None:
+                early_stopping_index = max_iteration
             residual = self.calculate_residual(
                 models[run].response,
                 models[run].design,
-                models[run].conjugate_gradient_estimate,
+                models[run].get_estimate(early_stopping_index),
             )
 
             self.assertAlmostEqual(
                 residual,
-                models[run].residuals[int(models[run].early_stopping_index)],
+                models[run].residuals[int(early_stopping_index)],
                 places=5,
             )
 
     def test_interpolation(self):
         # Test several properties of the interpolated conjugate gradients algorithm
+        critical_value = (np.sqrt(self.sample_size) + self.sample_size) * (self.noise_level**2)
         models = [
             es.ConjugateGradients(
                 self.design,
                 self.observation[:, i],
                 true_signal=self.signal,
                 true_noise_level=self.noise_level,
-                interpolation=True,
                 computation_threshold=0,
             )
             for i in range(self.NUMBER_RUNS)
         ]
 
+        max_iteration = self.sample_size
+        interpolation = True
         for run in range(self.NUMBER_RUNS):
-            models[run].discrepancy_stop(self.max_iter)
-            interpolated_residual = models[run].calculate_interpolated_residual(models[run].early_stopping_index)
+            early_stopping_index = models[run].get_discrepancy_stop(critical_value, max_iteration, interpolation)
+            if early_stopping_index is None:
+                early_stopping_index = max_iteration
+            interpolated_residual = models[run].get_residual(early_stopping_index)
 
             # Test if the interpolated squared residual at the discrepancy stopping index agrees with the critical value
-            if models[run].early_stopping_index < self.max_iter:
-                self.assertAlmostEqual(interpolated_residual, models[run].critical_value, places=5)
+            if early_stopping_index < max_iteration:
+                self.assertAlmostEqual(interpolated_residual, critical_value, places=5)
 
             interpolated_residual_via_estimator = self.calculate_residual(
                 models[run].response,
                 models[run].design,
-                models[run].conjugate_gradient_estimate,
+                models[run].get_estimate(early_stopping_index),
             )
 
             # Test if the interpolated squared residual at the discrepancy stopping index agrees with the squared residual of the conjugate gradient estimate at the same index
             self.assertAlmostEqual(interpolated_residual_via_estimator, interpolated_residual, places=5)
 
-            interpolated_strong_empirical_error = models[run].calculate_interpolated_strong_empirical_error(
-                models[run].early_stopping_index
-            )
-            interpolated_strong_empirical_error_via_estimator = np.sum(
-                (models[run].conjugate_gradient_estimate - models[run].true_signal) ** 2
-            )
-
-            # Test if the interpolated strong empirical error at the discrepancy stopping index agrees with the strong empirical error of the conjugate gradient estimate at the same index
-            self.assertAlmostEqual(
-                interpolated_strong_empirical_error_via_estimator,
-                interpolated_strong_empirical_error,
-                places=4,
-            )
-
-            interpolated_weak_empirical_error = models[run].calculate_interpolated_weak_empirical_error(
-                models[run].early_stopping_index
-            )
-            interpolated_weak_empirical_error_via_estimator = np.sum(
-                (models[run].design @ (models[run].conjugate_gradient_estimate - models[run].true_signal)) ** 2
-            )
-
-            # Test if the interpolated weak empirical error at the discrepancy stopping index agrees with the weak empirical error of the conjugate gradient estimate at the same index
-            self.assertAlmostEqual(
-                interpolated_weak_empirical_error_via_estimator,
-                interpolated_weak_empirical_error,
-                places=5,
-            )
-
     def test_early_stopping_index(self):
         # Test if the discrepancy stopping index for the model without interpolation agrees with the rounded up discrepancy stopping index for the interpolated model
-        models_interpolated = [
+        critical_value = (np.sqrt(self.sample_size) + self.sample_size) * (self.noise_level**2)
+        models = [
             es.ConjugateGradients(
                 self.design,
                 self.observation[:, i],
                 true_signal=self.signal,
                 true_noise_level=self.noise_level,
-                interpolation=True,
-                computation_threshold=0,
-            )
-            for i in range(self.NUMBER_RUNS)
-        ]
-        models_noninterpolated = [
-            es.ConjugateGradients(
-                self.design,
-                self.observation[:, i],
-                true_signal=self.signal,
-                true_noise_level=self.noise_level,
-                interpolation=False,
                 computation_threshold=0,
             )
             for i in range(self.NUMBER_RUNS)
         ]
 
+        max_iteration = self.sample_size
         for run in range(self.NUMBER_RUNS):
-            models_interpolated[run].discrepancy_stop(self.max_iter)
-            models_noninterpolated[run].discrepancy_stop(self.max_iter)
-            early_stopping_index_interpolated = models_interpolated[run].early_stopping_index
-            early_stopping_index_noninterpolated = models_noninterpolated[run].early_stopping_index
+            early_stopping_index_interpolated = models[run].get_discrepancy_stop(critical_value, max_iteration, True)
+            if early_stopping_index_interpolated is None:
+                early_stopping_index_interpolated = max_iteration
+            early_stopping_index_noninterpolated = models[run].get_discrepancy_stop(
+                critical_value, max_iteration, False
+            )
+            if early_stopping_index_noninterpolated is None:
+                early_stopping_index_noninterpolated = max_iteration
             self.assertAlmostEqual(
                 np.ceil(early_stopping_index_interpolated),
                 early_stopping_index_noninterpolated,
