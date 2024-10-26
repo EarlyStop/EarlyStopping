9af002051f0352d3c58993418f7c078d2737ffe7
ratmir-miftachov <miftachr@hu-berlin.de>
2024-10-26 19:50:15 -0400

    Now very similar to landweber

--- examples/regression_tree_example.py
+++ examples/regression_tree_example.py
@@ -15,8 +15,8 @@ importlib.reload(regression_tree)
 
 
 # Setting:
-n_train = 1000
-d = 5
+n_train = 1000 # sample size
+d = 5 # dimension
 noise_level = 1
 X_train = np.random.uniform(0, 1, size=(n_train, d))
 X_test = np.random.uniform(0, 1, size=(n_train, d))
@@ -32,6 +32,7 @@ es_regression_tree = regression_tree.DecisionTreeRegressor(design=X_train, respo
                                                            true_signal=f,
                                                            true_noise_vector=noise_train)
 
+# Iterate until max_depth! theoretical quantities are saved automatically.
 es_regression_tree.iterate(X_train, y_train, max_depth=20)
 
 
@@ -41,10 +42,10 @@ es_regression_tree.variance
 es_regression_tree.risk
 es_regression_tree.residuals
 
+#Get the stopping iterations:
 tau = es_regression_tree.get_discrepancy_stop(critical_value=0.8)
 balanced_oracle_iteration = es_regression_tree.get_balanced_oracle()
 
-
-prediction = es_regression_tree.predict(X_train, depth=tau) # TODO: Double check if tau and the prediction depth match here. tau and balanced_oracle_iteration do match! just something in prediciton weird.
+prediction = es_regression_tree.predict(X_train, depth=tau)
 
 
