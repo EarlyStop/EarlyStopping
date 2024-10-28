2f4d6e75aa3fb6ae12431c8c39b5966175983940
ratmir-miftachov <miftachr@hu-berlin.de>
2024-10-28 12:55:14 -0400

    adjusted .risk and .residuals

--- examples/plot_regression_tree_usage.py
+++ examples/plot_regression_tree_usage.py
@@ -1,5 +1,5 @@
 """
-Usage example for global early stopping for the regression tree
+Usage example for regression trees estimation
 ===============================================================
 
 We illustrate the usage and available methods of the regression tree class via a
@@ -13,6 +13,7 @@ for name in list(globals()):
 import importlib
 import numpy as np
 import EarlyStopping as es
+import matplotlib.pyplot as plt
 importlib.reload(es)
 
 
@@ -23,46 +24,82 @@ importlib.reload(es)
 sample_size = 1000
 para_size = 5
 true_noise_level = 1
-X_train = np.random.uniform(0, 1, size=(sample_size, para_size))
-X_test = np.random.uniform(0, 1, size=(sample_size, para_size))
+design = np.random.uniform(0, 1, size=(sample_size, para_size))
 
-def generate_rectangular(X, noise_level, add_noise=True):
-    n = X.shape[0]
+def generate_rectangular(design, noise_level, add_noise=True):
+    sample_size = design.shape[0]
     if add_noise:
-        noise = np.random.normal(0, noise_level, n)
+        noise = np.random.normal(0, noise_level, sample_size)
     else:
-        noise = np.zeros(n)
+        noise = np.zeros(sample_size)
 
-    # For X uniform:
-    y_temp = ((1 / 3 <= X[:, 0]) * (X[:, 0] <= 2 * 1 / 3) * (1 / 3 <= X[:, 1]) * (X[:, 1] <= 2 * 1 / 3))
+    response_temp = ((1 / 3 <= design[:, 0]) * (design[:, 0] <= 2 * 1 / 3) * (1 / 3 <= design[:, 1]) * (design[:, 1] <= 2 * 1 / 3))
+    response = response_temp.astype(int) + noise
 
-    y = y_temp.astype(int) + noise
+    return response, noise
+
+def generate_smooth_signal(design, noise_level, add_noise=True):
+    sample_size = design.shape[0]
+    if add_noise:
+        noise = np.random.normal(0, noise_level, sample_size)
+    else:
+        noise = np.zeros(sample_size)
+
+    y = 20 * np.exp(-5 * ((design[:, 0] - 1 / 2) ** 2 + (design[:, 1] - 1 / 2) ** 2 - 0.9 * (design[:, 0] - 1 / 2) * (design[:, 1] - 1 / 2))) + noise
+
     return y, noise
 
+def generate_sine_cosine(design, noise_level, add_noise=True):
+    sample_size = design.shape[0]
+    if add_noise:
+        noise = np.random.normal(0, noise_level, sample_size)
+    else:
+        noise = np.zeros(sample_size)
+    y = np.sin(3 * np.pi * design[:, 0]) + np.cos(5 * np.pi * design[:, 1]) + noise
+    return y, noise
 
-
-response_train, noise_train = generate_rectangular(X_train, true_noise_level, add_noise=True)
-response_test, noise_test = generate_rectangular(X_test, true_noise_level, add_noise=True)
-f, _ = generate_rectangular(X_train, true_noise_level, add_noise=False)
+response, noise = generate_sine_cosine(design, true_noise_level, add_noise=True)
+f, _ = generate_sine_cosine(design, true_noise_level, add_noise=False)
 
 
 # %%
-# Initialize RegressionTree class
-alg = es.RegressionTree(design=X_train, response=response_train, min_samples_split=1, true_signal=f,
-                        true_noise_vector=noise_train)
+# Theoretical bias-variance decomposition
+# ------------------------------
+# Decompose the risk into squared bias and variance.
+alg = es.RegressionTree(design=design, response=response, min_samples_split=1, true_signal=f,
+                        true_noise_vector=noise)
 alg.iterate(max_depth=20)
 
-# Bias-variance decomposition and oracle quantities
+# Bias-variance decomposition
 alg.bias2
 alg.variance
-alg.risk
+
+plt.figure(figsize=(8, 6))
+plt.plot(alg.risk, marker='o', label='Risk')
+plt.plot(alg.bias2, marker='o', label='Bias')
+plt.plot(alg.variance, marker='o', label='Variance')
+plt.xlabel('Generation')
+plt.grid(True)
+plt.legend()
+plt.show()
+
+
 alg.residuals
 
-# Early stopping w/ discrepancy principle
+
+
+
+# %%
+# Early stopping via the discrepancy principle
+# --------------------------------------------------
+# Stop the breadth-first search growth of the tree when the residuals become smaller than the critical value.
 tau = alg.get_discrepancy_stop(critical_value=1)
 balanced_oracle_iteration = alg.get_balanced_oracle()
 
+# %%
 # Prediction
-prediction = alg.predict(X_train, depth=10)
+# ------------
+# The class has a method to predict the response for new design points.
+prediction = alg.predict(design, depth=10)
 
 
