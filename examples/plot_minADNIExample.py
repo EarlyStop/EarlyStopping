3961abf8c10f459e97c42021c3fdb63cb21827d3
ziebelle <32039090+ziebelle@users.noreply.github.com>
2025-10-08 10:13:30 +0100

    Add comments for file opening in plot_minADNIExample.py
    Added comments to clarify the file opening process.

--- examples/plot_minADNIExample.py
+++ examples/plot_minADNIExample.py
@@ -13,6 +13,11 @@ import gnupg
 sns.set_theme()
 password =  os.environ.get("PASSWORD")
 
+# %%
+# Opening the file
+# -------------------------
+# Here we open the csv file
+
 gpg = gnupg.GPG()
 with open("testData.gpg", "rb") as f:
     result = gpg.decrypt_file(f, passphrase=password)
