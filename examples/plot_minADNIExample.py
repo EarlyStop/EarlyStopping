26f23118a09c680cfa875874d5469a817a91556c
be5tan <be5tan@posteo.de>
2025-10-08 11:48:43 +0200

    Started minimal ADNI example for docu...

--- examples/plot_minADNIExample.py
+++ examples/plot_minADNIExample.py
@@ -3,17 +3,22 @@ A minimal example for the ADNI application
 ==========================================
 """
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import EarlyStopping as es
 import gnupg
 
+
 sns.set_theme()
 
+if os.path.exists("testData.gpg"):
+    print("File exists!")
 
 gpg = gnupg.GPG()
 with open("testData.gpg", "rb") as f:
-    result = gpg.decrypt_file(f, passphrase="Test")
+    result = gpg.decrypt_file(f, passphrase="HeadPhoneChalkEraserMat")
 
+type(result)
 print(result)
