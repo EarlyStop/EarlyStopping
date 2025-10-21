082c26d081d29aa1b9c21f38bb7f538fd540ed69
be5tan <be5tan@posteo.de>
2025-10-21 18:15:27 +0200

    Minimal password example.

--- examples/plot_minADNIExample.py
+++ examples/plot_minADNIExample.py
@@ -9,25 +9,6 @@ import pandas as pd
 from io import StringIO
 
 
-password = os.environ.get("PASSWORD")  # safer than hardcoding
+password = os.environ.get("PASSWORD")
 print(password)
-
-# Path to your encrypted file
-encrypted_file = "testData.gpg"
-
-
-# Run GPG and capture the decrypted output
-result = subprocess.run(
-    ["gpg", "--batch", "--yes", "--passphrase", password, "--decrypt", encrypted_file],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    check=True
-)
-
-# Convert decrypted bytes to text
-decrypted_text = result.stdout.decode("utf-8")
-
-# Load the CSV content into a DataFrame
-df = pd.read_csv(StringIO(decrypted_text))
-
-print(df.head())
+password
