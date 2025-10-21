73a357a01c893eef677a5a7846232f046b25d02d
be5tan <be5tan@posteo.de>
2025-10-21 19:29:59 +0200

    Final attempt dataframe.

--- examples/plot_minADNIExample.py
+++ examples/plot_minADNIExample.py
@@ -8,23 +8,43 @@ import subprocess
 import pandas as pd
 from io import StringIO
 
-# Path to your encrypted file
-encrypted_file = "testData.gpg"
 
 password = os.environ.get("PASSWORD")
 
 # Run GPG and capture the decrypted output
 result = subprocess.run(
-    ["gpg", "--batch", "--yes", "--passphrase", password, "--decrypt", encrypted_file],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    check=True
+    [
+        "gpg",
+        "--batch",              # no interactive prompts
+        "--yes",                # assume "yes" on overwrite
+        "--passphrase", password,
+        "--decrypt", "testData.gpg",
+    ],
+    capture_output=True,
+    check=True,
 )
 
-# Convert decrypted bytes to text
-decrypted_text = result.stdout.decode("utf-8")
+type(result)
+type(result.stdout.decode("utf-8"))
 
-# Load the CSV content into a DataFrame
-df = pd.read_csv(StringIO(decrypted_text))
+df = pd.read_csv(StringIO(result.stdout.decode("utf-8")))
 
 print(df.head())
+
+# Path to your encrypted file
+# encrypted_file = "testData.gpg"
+
+# result = subprocess.run(
+#     ["gpg", "--batch", "--yes", "--passphrase", password, "--decrypt", encrypted_file],
+#     stdout=subprocess.PIPE,
+#     stderr=subprocess.PIPE,
+#     check=True
+# )
+# 
+# # Convert decrypted bytes to text
+# decrypted_text = result.stdout.decode("utf-8")
+# 
+# # Load the CSV content into a DataFrame
+# df = pd.read_csv(StringIO(decrypted_text))
+# 
+# print(df.head())
