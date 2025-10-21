9997e31b9de5000f6a4cd78652dddcf47e0c5aa8
be5tan <be5tan@posteo.de>
2025-10-21 19:00:19 +0200

    Check KEY.

--- examples/plot_minADNIExample.py
+++ examples/plot_minADNIExample.py
@@ -9,17 +9,17 @@ import pandas as pd
 from io import StringIO
 
 
-password1 = os.environ["PASSWORD"]
-print(password1)
-password1
-
-password2 = os.environ.get("PASSWORD")
-print(password2)
-password2
-
-key2 = os.environ["KEY"]
-print(key2)
-key2
+# password1 = os.environ["PASSWORD"]
+# print(password1)
+# password1
+# 
+# password2 = os.environ.get("PASSWORD")
+# print(password2)
+# password2
+# 
+# key2 = os.environ["KEY"]
+# print(key2)
+# key2
 
 key1 = os.environ.get("KEY")
 print(key1)
