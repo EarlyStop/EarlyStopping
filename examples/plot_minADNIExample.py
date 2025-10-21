05477d39e9968375b03afd98c6b89e01f3712e73
be5tan <be5tan@posteo.de>
2025-10-21 17:57:58 +0200

    Corrected passphrase processing test.

--- examples/plot_minADNIExample.py
+++ examples/plot_minADNIExample.py
@@ -9,30 +9,25 @@ import pandas as pd
 from io import StringIO
 
 
-password = os.environ["PASSWORD"]  # safer than hardcoding
+password = os.environ.get("PASSWORD")  # safer than hardcoding
 print(password)
 
 # Path to your encrypted file
 encrypted_file = "testData.gpg"
 
-if os.path.exists(encrypted_file):
-    print("File exists!")
-else:
-    print("File not found!")
 
-# 
-# # Run GPG and capture the decrypted output
-# result = subprocess.run(
-#     ["gpg", "--batch", "--yes", "--passphrase", password, "--decrypt", encrypted_file],
-#     stdout=subprocess.PIPE,
-#     stderr=subprocess.PIPE,
-#     check=True
-# )
-# 
-# # Convert decrypted bytes to text
-# decrypted_text = result.stdout.decode("utf-8")
-# 
-# # Load the CSV content into a DataFrame
-# df = pd.read_csv(StringIO(decrypted_text))
-# 
-# print(df.head())
+# Run GPG and capture the decrypted output
+result = subprocess.run(
+    ["gpg", "--batch", "--yes", "--passphrase", password, "--decrypt", encrypted_file],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    check=True
+)
+
+# Convert decrypted bytes to text
+decrypted_text = result.stdout.decode("utf-8")
+
+# Load the CSV content into a DataFrame
+df = pd.read_csv(StringIO(decrypted_text))
+
+print(df.head())
