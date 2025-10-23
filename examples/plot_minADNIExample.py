022eae0f9fb5fb96cd8fcb0324571ec2eb111f65
be5tan <be5tan@posteo.de>
2025-10-23 14:28:07 +0200

    Created examples/data directory.

--- examples/plot_minADNIExample.py
+++ examples/plot_minADNIExample.py
@@ -10,8 +10,6 @@ from pathlib import Path
 
 password = os.environ.get("PASSWORD")
 
-#gpg_file = Path(__file__).with_name("testData.gpg")  # examples/testData.gpg
-
 result = subprocess.run(
     [
         "gpg",
@@ -21,7 +19,7 @@ result = subprocess.run(
         "--passphrase", 
         password,
         "--decrypt", 
-        "testData.gpg",
+        "data/testData.gpg",
     ],
     capture_output=True,
     check=True,
@@ -30,55 +28,3 @@ result = subprocess.run(
 
 df = pd.read_csv(StringIO(result.stdout))
 print(df.head())
-
-
-# """
-# A minimal example for the ADNI application
-# ==========================================
-# """
-
-# import os
-# import subprocess
-# import pandas as pd
-# from io import StringIO
-
-
-# password = os.environ.get("PASSWORD")
-
-# # Run GPG and capture the decrypted output
-# result = subprocess.run(
-#     [
-#         "gpg",
-#         "--batch",              # no interactive prompts
-#         "--yes",                # assume "yes" on overwrite
-#         "--passphrase", password,
-#         "--decrypt", "testData.gpg",
-#     ],
-#     capture_output=True,
-#     check=True,
-# )
-
-# type(result)
-# type(result.stdout.decode("utf-8"))
-
-# df = pd.read_csv(StringIO(result.stdout.decode("utf-8")))
-
-# print(df.head())
-
-# Path to your encrypted file
-# encrypted_file = "testData.gpg"
-
-# result = subprocess.run(
-#     ["gpg", "--batch", "--yes", "--passphrase", password, "--decrypt", encrypted_file],
-#     stdout=subprocess.PIPE,
-#     stderr=subprocess.PIPE,
-#     check=True
-# )
-# 
-# # Convert decrypted bytes to text
-# decrypted_text = result.stdout.decode("utf-8")
-# 
-# # Load the CSV content into a DataFrame
-# df = pd.read_csv(StringIO(decrypted_text))
-# 
-# print(df.head())
